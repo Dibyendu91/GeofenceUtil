2620366a191311960a11c449650718640f04bef2
Dibyendu Das <dibyendudas.mail@gmail.com>
2020-09-05 21:51:22 +0530

    New App Centre key updated.

--- app/src/main/java/com/dibyendu/gefenceutil/util/AppConstant.kt
+++ app/src/main/java/com/dibyendu/gefenceutil/util/AppConstant.kt
@@ -3,7 +3,7 @@ package com.dibyendu.gefenceutil.util
 const val DATABASE_VERSION = 1
 const val DATABASE_NAME = "geofence.db"
 const val GEOFENCE_RADIUS_IN_METERS = 500f
-const val APP_CENTER_KEY = "16a83511-5769-409e-b851-6d410af84a08"
+const val APP_CENTER_KEY = "3d1498fc-57ae-445c-b29a-e05b0238f447"
 
 enum class Type{
     NAME,
